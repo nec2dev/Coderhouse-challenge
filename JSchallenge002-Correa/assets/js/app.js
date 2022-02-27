371a32d27501becaf51b9eae81e5383846fd528e
Nahuel Correa <nec2multivackergithub@outlook.com>
2022-02-27 15:43:34 -0300

    variables created

--- JSchallenge002-Correa/assets/js/app.js
+++ JSchallenge002-Correa/assets/js/app.js
@@ -1,7 +1,12 @@
 function newClientQuote() {
-    m2 = parseInt(prompt("Ingrese los M2 de la propiedad"))
-    zona = parseInt(prompt("Ingrese en qué zona vive:"))
-    const cotizadorSeguroHogar = new Cotizador(m2, valorMetro2Seguro, zona)
-    res = cotizadorSeguroHogar.cotizarPoliza()
-    console.log("El valor de la póliza es: ARS", res)
+    nec2quote = parseFloat(prompt("Ingrese el valor de la solución 0"));
+    alkemiquote = parseFloat(prompt("Ingrese el valor de la solución 1"));
+    aparzeroquote = parseFloat(prompt("Ingrese el valor de la solución 2"));
+    albatrossquote = parseFloat(prompt("Ingrese el valor de la solución 3"));
+    arquitepiaquote = parseFloat(prompt("Ingrese el valor de la solución 4"));
+    abitatquote = parseFloat(prompt("Ingrese el valor de la solución 5"));
+    alephandriaquote = parseFloat(prompt("Ingrese el valor de la solución 6"));
+    const solutionsQuoter = new finalQuoter(nec2quote, alkemiquote, aparzeroquote, albatrossquote, arquitepiaquote, abitatquote, alephandriaquote);
+    resultFinalQuote = solutionsQuoter.quoteSolutions();
+    console.log(resultFinalQuote);
 }
--- JSchallenge002-Correa/assets/js/quoteGen.js
+++ JSchallenge002-Correa/assets/js/quoteGen.js
@@ -1,15 +1,15 @@
 class finalQuoter {
     constructor(nec2quote, alkemiquote, aparzeroquote, albatrossquote, arquitepiaquote, abitatquote, alephandriaquote) {
-        this.metros2 = nec2quote;
-        this.valorM2 = alkemiquote;
-        this.zonaResidencia = aparzeroquote;
-        this.metros2 = albatrossquote;
-        this.valorM2 = arquitepiaquote;
-        this.zonaResidencia = abitatquote;
-        this.zonaResidencia = alephandriaquote;
+        this.sol0quote = nec2quote;
+        this.sol1quote = alkemiquote;
+        this.sol2quote = aparzeroquote;
+        this.sol3quote = albatrossquote;
+        this.sol4quote = arquitepiaquote;
+        this.sol5quote = abitatquote;
+        this.sol6quote = alephandriaquote;
     }
-    cotizarPoliza() {
-        let finalQuote = this.nec2quote + this.alkemiquote + this.aparzeroquote + this.albatrossquote + this.arquitepiaquote + this.abitatquote + this.alephandriaquote;
+    quoteSolutions() {
+        let finalQuote = this.sol0quote + this.sol1quote + this.sol2quote + this.sol3quote + this.sol4quote + this.sol5quote + this.sol6quote;
         return finalQuote;
     }
 }
